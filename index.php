69cf39724222f5e047ea5e4956b2180c71c653d6
music5272 <104003432+music5272@users.noreply.github.com>
2022-11-06 14:10:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form>
+            <input type="text" name="username">
+            <input type="password" name="password">
+        </form>
+    </body>
+</html>
